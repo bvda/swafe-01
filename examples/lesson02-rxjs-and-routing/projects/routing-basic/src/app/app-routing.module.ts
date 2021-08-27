087f4b9efb42deb5eec07bb1c586aed32addac26
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2021-08-27 22:52:36 +0200

    Add example code

---
+++ examples/lesson02-rxjs-and-routing/projects/routing-basic/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+const routes: Routes = [{
+  path: 'first',
+  component: FirstComponent,
+}, {
+  path: 'second',
+  component: SecondComponent,
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
