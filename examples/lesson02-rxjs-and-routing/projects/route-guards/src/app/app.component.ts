0dec37e979f128e8e87ad31604132fd1482fa130
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2021-09-02 23:00:03 +0200

    Add auth service implementation

--- examples/lesson02-rxjs-and-routing/projects/route-guards/src/app/app.component.ts
+++ examples/lesson02-rxjs-and-routing/projects/route-guards/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { iif, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'route-guards';
+
+  authed$: Observable<boolean>
+
+  constructor(private auth: AuthService) {
+  }
+  
+  ngOnInit() {
+    this.authed$ = this.auth.isAuthenticated()
+  }
+
+  signIn() {
+    this.auth.signIn()
+  }
+
+  signOut() {
+    this.auth.signOut()
+  }
 }
