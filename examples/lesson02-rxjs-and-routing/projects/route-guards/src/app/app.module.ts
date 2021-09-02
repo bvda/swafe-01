2ba75b981ab3aaeecc3cc4651fadee9aa31dd0e9
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2021-09-02 22:17:23 +0200

    Add component and service scaffolds

--- examples/lesson02-rxjs-and-routing/projects/route-guards/src/app/app.module.ts
+++ examples/lesson02-rxjs-and-routing/projects/route-guards/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicComponent } from './public/public.component';
+import { RestrictedComponent } from './restricted/restricted.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent,
+    RestrictedComponent
   ],
   imports: [
     BrowserModule,
