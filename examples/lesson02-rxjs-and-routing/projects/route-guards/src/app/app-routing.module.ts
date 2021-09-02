9eb3ef001493b2d35543ee811ad5f9d850b87783
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2021-09-02 22:27:58 +0200

    Add routes and outlets

--- examples/lesson02-rxjs-and-routing/projects/route-guards/src/app/app-routing.module.ts
+++ examples/lesson02-rxjs-and-routing/projects/route-guards/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './public/public.component';
+import { RestrictedComponent } from './restricted/restricted.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  pathMatch: 'full',
+  children: [{
+    path: '',
+    component: PublicComponent
+  }, {
+    path: '',
+    component: RestrictedComponent,
+    outlet: 'authed'
+  }]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
