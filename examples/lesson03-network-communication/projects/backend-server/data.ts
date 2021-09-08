f78cac3db595efad3556caeb622ba43094d52eaa
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2021-09-08 10:01:54 +0200

    Add test data

--- examples/lesson03-network-communication/projects/backend-server/data.ts
+++ examples/lesson03-network-communication/projects/backend-server/data.ts
@@ -104,3 +104,54 @@ export const LAUNCH_VEHICLES = [{
     total: 110
   } 
 }]
+
+export const ASTRONAUTS = [{
+  name: "Neil Armstrong",
+  nationality: "US",
+  organization: "NASA",
+  born: 19300805,
+  died: 20120825,
+  time_in_space: 742350,
+}, {
+  name: "Scott Kelly",
+  nationality: "US",
+  organization: "NASA",
+  status: "retired",
+  born: 19640221,
+  time_in_space: 44928000,
+}, {
+  name: "Andreas Mogensen",
+  nationality: "DA",
+  organization: "ESA",
+  status: "active",
+  born: 19761102,
+  time_in_space: 850440
+}, {
+  name: "Andreas Mogensen",
+  nationality: "DA",
+  organization: "ESA",
+  status: "active",
+  born: 19761102,
+  time_in_space: 850440  
+}, {
+  name: "Chris Hadfield",
+  nationality: "CA",
+  organization: "NASA",
+  status: "retired",
+  born: 19590829,
+  time_in_space: 14342400
+}, {
+  name: "Mark Kelly",
+  nationality: "US",
+  organization: "NASA",
+  status: "retired",
+  born: 19640221,
+  time_in_space: 4673040,  
+}, {
+  name: "Yang Liwei",
+  nationality: "ZH",
+  organization: "PLAAC",
+  status: "retired",
+  born: 19650621,
+  time_in_space: 76965,    
+}]
