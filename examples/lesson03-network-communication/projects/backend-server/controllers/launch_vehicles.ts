f7f1253117900e3e199c2836cc431f5098246c48
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2021-09-11 00:56:11 +0200

    Add error-handling example project scaffold

--- examples/lesson03-network-communication/projects/backend-server/controllers/launch_vehicles.ts
+++ examples/lesson03-network-communication/projects/backend-server/controllers/launch_vehicles.ts
@@ -4,6 +4,5 @@ export const list = function(req, res) {
   res.json(LAUNCH_VEHICLES)  
 }
 export const update = function(req, res) {
-  var launchVehicle = req.body.launch_vehicle
   res.send({"success": true})
 }
--- examples/lesson03-network-communication/projects/backend-server/index.ts
+++ examples/lesson03-network-communication/projects/backend-server/index.ts
@@ -1,12 +1,14 @@
 import * as express from 'express';
 import { router as corsRouter } from './routers/cors'
 import { router as apiRouter } from './routers/api'
+import { router as faultyRouter} from './routers/faulty'
 
 const app = express();
 const port = 3000;
 
 app.use(express.json())
 
+app.use('/faulty', faultyRouter)
 app.use('/api', apiRouter)
 app.use('/cors', corsRouter)
 
