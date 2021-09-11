97e6c2b5604acac3842e7a7ff9e11073db71b269
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2021-09-11 20:44:34 +0200

    Add catchError slide

--- examples/lesson03-network-communication/projects/error-handling/src/app/app.component.ts
+++ examples/lesson03-network-communication/projects/error-handling/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Astronaut } from 'lib-space';
+import { Observable, of } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { SpaceError } from './space-error.type';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'error-handling';
+export class AppComponent implements OnInit {
+  faultyAstronauts$: Observable<Astronaut[] | SpaceError> | null = null
+  error = ''
+
+  constructor(private http: HttpClient) { }
+  ngOnInit() {
+    this.faultyAstronauts$ = this.http.get<Astronaut[]>('http://localhost:3000/faulty/astronauts').pipe(
+      catchError((e: HttpErrorResponse) => {
+        console.error(e.message)
+        if(e.status === 0) {
+          // Client-side or network error
+          console.error(e.message)
+          this.error = 'Client-side or network error'
+        } else {
+          // Endpoint returned an unsuccessful response code
+          this.error = 'Endpoint returned an unsuccessful response code'
+        }
+        return of({ message: e.message, status_code: e.status });
+      }),
+    )
+  }
 }
