5b558ad1874f09674f8495b5bf448b6e832469f4
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2021-09-17 08:50:35 +0200

    Removed lesson04 stuff

--- examples/lesson04-forms/projects/warcraft/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of warcraft
- */
-
-export * from './lib/warcraft.service';
-export * from './lib/warcraft.component';
-export * from './lib/warcraft.module';
-
-export * from './lib/class.type';
