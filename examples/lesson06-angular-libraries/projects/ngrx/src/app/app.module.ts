0cccd01c3ef172439846bad5a6b545fb0ae1f3a4
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2021-09-28 18:04:50 +0200

    Add stuff

--- examples/lesson06-angular-libraries/projects/ngrx/src/app/app.module.ts
+++ examples/lesson06-angular-libraries/projects/ngrx/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { deviceReducer } from './state/device.reducer';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ devices: deviceReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
