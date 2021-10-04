34c7cfd380e28b0822bd93019552203664f06526
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2021-10-04 10:11:36 +0200

    Fix imports and refactoring

--- exercises/lesson06-exercises/src/app/app-routing.module.ts
+++ exercises/lesson06-exercises/src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HostsComponent } from './hosts/hosts.component';
-import { NetworkLogComponent } from './network-log/network-log.component';
+import { AccessLogComponent } from './access-log/access-log.component';
 
 const routes: Routes = [{
   path: '',
   component: HomeComponent
 }, {
-  path: 'network-log',
-  component: NetworkLogComponent
+  path: 'access-log',
+  component: AccessLogComponent
 }, {
   path: 'hosts',
   component: HostsComponent
