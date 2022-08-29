9424396276a3f99b8fba9607b4f725bd0a3abf25
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-08-29 15:06:11 +0200

    Fix wording

--- solutions/lesson01-introduction/projects/models/src/public-api.ts
+++ solutions/lesson01-introduction/projects/models/src/public-api.ts
@@ -2,7 +2,4 @@
  * Public API Surface of models
  */
 
-export * from './lib/models.service';
-export * from './lib/models.component';
-export * from './lib/models.module';
 export * from './lib/course.type';
