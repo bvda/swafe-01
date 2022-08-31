742c4ed22127d8cdc79a64b91af3b5eab22cc20d
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-08-31 10:20:07 +0200

    Fixed (some) error handling

--- exercises/lesson-02-exercises.md
+++ exercises/lesson-02-exercises.md
@@ -56,20 +56,26 @@ export class BillionaireService {
   
   billionaires: Billionaire[] = [{ 
     name: 'Bill Gates',
-    net_worth: 133200000000,
+    net_worth: 111000000000,
     nationality: 'US',
   }, {
     name: 'Jeff Bezos',
-    net_worth: 200100000000,
+    net_worth: 155300000000,
     nationality: 'US',
   }, {
     name: 'Elon Musk',
-    net_worth: 190700000000,
+    net_worth: 257300000000,
     nationality: 'US',
   }, {
     name: 'Mark Zuckerberg',
-    net_worth: 135200000000,
+    net_worth: 58200000000,
     nationality: 'US',
+  }, {
+    name: 'Russ Hanneman',
+    net_worth: 986000000
+  }, {
+    name: 'Jack Ma',
+    net_worth: 24000000000
   }]
 }
 
--- solutions/lesson02-rxjs-and-routing/src/app/user/details/details.component.ts
+++ solutions/lesson02-rxjs-and-routing/src/app/user/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { catchError, Observable, switchMap } from 'rxjs';
+import { Billionaire, BillionaireService } from 'src/app/billionaire.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  billionaire$!: Observable<Billionaire | unknown>
+
+  constructor(private service: BillionaireService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    try {
+      this.billionaire$ = this.activatedRoute.paramMap.pipe(switchMap(c => {
+          return this.service.getBillionaire(c.get('id') ?? "");
+        }),
+        catchError((c) => c))
+    } catch (e: any) {
+      console.log(e)
+    }
   }
 
 }
