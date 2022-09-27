aae99ea6c19f067f38f8429f7d42f35dcd366cf9
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-09-27 09:36:25 +0200

    Add solution proposal for lesson 05

--- solutions/lesson05-login-screen/src/app/app.module.ts
+++ solutions/lesson05-login-screen/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule
