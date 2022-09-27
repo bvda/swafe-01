b0e417c070c539d96ec96212d4903d23206d8d61
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-09-27 12:47:47 +0200

    Add stuff

--- solutions/lesson04-forms/src/app/reactive/form/form.component.ts
+++ solutions/lesson04-forms/src/app/reactive/form/form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { combineLatest, map, merge, Observable } from 'rxjs';
+import { Class } from 'src/app/class.type';
+import { Race } from 'src/app/race.type';
+import { WarcraftService } from 'src/app/warcraft.service';
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  form = this.fb.group({
+    name: ['', [Validators.required]],
+    password: [''],
+    confirm_password: [''],
+    race: ['', [Validators.required]],
+    class: ['', [Validators.required]],
+    level: ['', [Validators.min(1), Validators.max(60)]],
+    description: [''],
+  })
+
+  classes$: Observable<Class[]>
+  races$: Observable<Race[]>
+
+  constructor(private fb: FormBuilder, private service: WarcraftService) { 
+    this.classes$ = this.service.getClasses()
+    this.races$ = combineLatest([this.service.getAllianceRaces(), this.service.getHordeRaces()]).pipe(map(c => [...c[0], ...c[1]]))
+  }
 
   ngOnInit(): void {
   }
+
+  onSubmit() {
+    console.log('onSubmit')
+  }
+
+  get name() { return this.form.get('name') }
 
 }
--- solutions/lesson04-forms/src/app/template/form/form.component.ts
+++ solutions/lesson04-forms/src/app/template/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
@@ -10,6 +11,11 @@ export class FormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    console.warn('onSubmit()')
+    console.log(form.value)
   }
 
 }
