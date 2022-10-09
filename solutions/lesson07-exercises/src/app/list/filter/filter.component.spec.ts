45a8d3336f26d8d283d75af0fe144bf15bd1933c
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2022-10-09 18:53:06 +0200

    Add select test

--- solutions/lesson07-exercises/src/app/list/filter/filter.component.spec.ts
+++ solutions/lesson07-exercises/src/app/list/filter/filter.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { FilterComponent } from './filter.component';
 
@@ -21,5 +22,17 @@ describe('FilterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should trigger change event', () => {
+    let de: HTMLSelectElement = fixture.debugElement.query(By.css('.car-make-select')).nativeElement; 
+    de.value = de.options[0].value;
+    de.dispatchEvent(new Event('change')) 
+    fixture.detectChanges();
+    fixture.whenStable().then(_ => {
+      let text = de.options[de.selectedIndex].label;
+      expect(text).toBe("Scion")
+    })
+    
   });
 });
